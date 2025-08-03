4a41cfcc58ebd56cfa03026d8e959865358f2872
HabibaSajid321 <90784842+HabibaSajid321@users.noreply.github.com>
2025-08-03 14:56:25 +0500

    langchain workshop

---
+++ chatbot.py
@@ -0,0 +1,22 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Get API key
+api_key = os.getenv("GOOGLE_API_KEY")
+
+# Initialize Gemini model
+chat = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature =  0.3, google_api_key=api_key)
+
+# Start a loop to chat
+print("Chatbot: Hello! Ask me anything. Type 'exit' to stop.")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+    response = chat.invoke([HumanMessage(content=user_input)])
+    print("Chatbot:", response.content)
